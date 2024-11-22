879e552588a90ecdb407c8a4c3bd8a02ba726be0
Jacky Lee <leeja12@oocl.com>
2024-11-22 17:37:50 +0800

    [Story6] feat: case2: return lot 1 when park given 2 lots and a super parking boy and lot1 being more free in percentage

--- src/test/java/com/parkinglot/SuperParkingBoyTest.java
+++ src/test/java/com/parkinglot/SuperParkingBoyTest.java
@@ -27,5 +27,21 @@ public class SuperParkingBoyTest {
         // Then
         assertTrue(parkingLot2.getParkingRecords().containsKey(ticket1));
     }
-    
+
+    @Test
+    void should_return_lot_one_parked_cars_when_park_given_two_lots_a_smart_parking_boy_and_a_car() throws Exception {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1,100);
+        ParkingLot parkingLot2 = new ParkingLot(2,100);
+        ParkingBoy parkingBoy = new ParkingBoy(new ArrayList<>(Arrays.asList(parkingLot2)));
+        SmartParkingBoy superParkingBoy = new SmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
+        Car car1 = new Car();
+        Car car2 = new Car();
+        parkingBoy.park(car1);
+        Ticket ticket1 = superParkingBoy.park(car2);
+        // When
+        // Then
+        assertTrue(parkingLot1.getParkingRecords().containsKey(ticket1));
+    }
+
 }
