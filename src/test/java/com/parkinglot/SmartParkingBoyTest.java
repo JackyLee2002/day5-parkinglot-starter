3127bb2a02aad5763dae7dcc3336a4ed5cba08a7
Jacky Lee <leeja12@oocl.com>
2024-11-22 17:30:02 +0800

    [Story5] feat: case 4-6, return nothing and error message when fetch/park given invalid/unused ticket or no free spot

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -50,4 +50,63 @@ public class SmartParkingBoyTest {
         // Then
         assertTrue(parkingLot1.getParkingRecords().containsKey(ticket1));
     }
+
+    @Test
+    void should_return_nothing_and_err_message_given_two_lots_equally_free_a_smart_parking_boy_and_a_car() throws Exception {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1,1);
+        ParkingLot parkingLot2 = new ParkingLot(2,1);
+        SmartParkingBoy parkingBoy = new SmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
+        Car car1 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        // When
+        // Then
+        assertTrue(parkingLot1.getParkingRecords().containsKey(ticket1));
+    }
+
+    @Test
+    void should_returned_nothing_with_err_message_when_fetch_given_a_parking_boy_and_two_lots_and_a_car_and_a_invalid_ticket() throws Exception {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1, 1);
+        ParkingLot parkingLot2 = new ParkingLot(2, 1);
+        SmartParkingBoy parkingBoy = new SmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
+        Car car1 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        Ticket unrecognizedTicket = new Ticket();
+        // When
+        // Then
+        assertThrows(UnrecognizedParkingTicketException.class, () -> parkingBoy.fetch(unrecognizedTicket), UNRECOGNIZED_PARKING_TICKET_ERROR_MESSAGE);
+    }
+
+    @Test
+    void should_returned_nothing_with_err_message_when_fetch_given_a_parking_boy_and_two_lots_and_a_car_and_a_used_ticket() throws Exception {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1, 1);
+        ParkingLot parkingLot2 = new ParkingLot(2, 1);
+        SmartParkingBoy parkingBoy = new SmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
+        Car car1 = new Car();
+        Ticket ticket = parkingBoy.park(car1);
+        Car fetchedCar = parkingBoy.fetch(ticket);
+        // When
+        // Then
+        assertEquals(car1, fetchedCar);
+        assertThrows(UnrecognizedParkingTicketException.class, () -> parkingBoy.fetch(ticket), UNRECOGNIZED_PARKING_TICKET_ERROR_MESSAGE);
+    }
+
+    @Test
+    void should_returned_nothing_with_err_message_when_park_given_a_parking_boy_and_two_full_lots_and_a_car() throws Exception {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1, 1);
+        ParkingLot parkingLot2 = new ParkingLot(2, 1);
+        SmartParkingBoy parkingBoy = new SmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        parkingBoy.park(car1);
+        parkingBoy.park(car2);
+        // When
+        // Then
+        assertThrows(NoAvailablePositionException.class, () -> parkingBoy.park(car3), NO_AVAILABLE_POSITION_MESSAGE);
+    }
+
 }
