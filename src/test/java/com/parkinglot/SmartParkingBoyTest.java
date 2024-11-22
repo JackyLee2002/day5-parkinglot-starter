82554d7e9f058c0e96af1d36bc3bf0935a8db209
Jacky Lee <leeja12@oocl.com>
2024-11-22 17:27:12 +0800

    [Story5] feat: case 3: return parked car at lot 1 when park given 2 lots equally free and a smart parking boy and a car

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -13,7 +13,7 @@ public class SmartParkingBoyTest {
     public static final String NO_AVAILABLE_POSITION_MESSAGE = "No available position.";
 
     @Test
-    void should_return_lot_two_parked_cars_when_park_given_two_lots_a_smart_parking_boy_and_two_cars_and_two_tickets() throws Exception {
+    void should_return_lot_two_parked_cars_when_park_given_two_lots_a_smart_parking_boy_and_a_car() throws Exception {
         // Given
         ParkingLot parkingLot1 = new ParkingLot(1,1);
         ParkingLot parkingLot2 = new ParkingLot(2,100);
@@ -26,9 +26,22 @@ public class SmartParkingBoyTest {
     }
 
     @Test
-    void should_return_lot_one_parked_cars_when_park_given_two_lots_a_smart_parking_boy_and_two_cars_and_two_tickets() throws Exception {
+    void should_return_lot_one_parked_cars_when_park_given_two_lots_a_smart_parking_boy_and_a_car() throws Exception {
         // Given
         ParkingLot parkingLot1 = new ParkingLot(1,100);
+        ParkingLot parkingLot2 = new ParkingLot(2,1);
+        SmartParkingBoy parkingBoy = new SmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
+        Car car1 = new Car();
+        Ticket ticket1 = parkingBoy.park(car1);
+        // When
+        // Then
+        assertTrue(parkingLot1.getParkingRecords().containsKey(ticket1));
+    }
+
+    @Test
+    void should_return_lot_one_parked_cars_when_park_given_two_lots_equally_free_a_smart_parking_boy_and_a_car() throws Exception {
+        // Given
+        ParkingLot parkingLot1 = new ParkingLot(1,1);
         ParkingLot parkingLot2 = new ParkingLot(2,1);
         SmartParkingBoy parkingBoy = new SmartParkingBoy(new ArrayList<>(Arrays.asList(parkingLot1, parkingLot2)));
         Car car1 = new Car();
